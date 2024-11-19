bf1cdb6c586a7dfdc17ddb09fb3e323025b3de4b
LiuMengQiang <lfc191910@gmail.com>
2024-11-19 18:13:38 +0800

    代码提交

--- src/modules/tenant/tenant.entity.ts
+++ src/modules/tenant/tenant.entity.ts
@@ -17,7 +17,26 @@ export class TenantEntity extends CompleteEntity{
   @ApiProperty({ description: '状态',default: 1 })
   status: string;
 
-  @Column()
-  @ApiProperty({ description: '预约次数统计',default: 0 })
-  booking_count: number;
+  // @Column()
+  // @ApiProperty({ description: '预约次数统计',default: 0 })
+  // booking_count: number;
+
+  @Column({  name: 'app_id',comment: '小程序appId' })
+  @ApiProperty({ description: '小程序appId' })
+  appId: string;
+
+  //域名
+  @Column({ name: 'domain_name',comment: '域名' })
+  @ApiProperty({ description: '域名' })
+  domainName:string;
+ 
+  //付款时间
+  @Column({ name: 'pay_time',comment: '付款时间' })
+  @ApiProperty({ description: '付款时间' })
+  payTime: Date;
+
+  //到期时间
+  @Column({ name: 'expire_time',comment: '到期时间' })
+  @ApiProperty({ description: '到期时间' })
+  expireTime: Date;
 }
